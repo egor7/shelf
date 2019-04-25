00da81b8ac127b37fceb045aa9dad7ff95e056c3
egor7 <egor.shalashnikov.bb@gmail.com>
2019-04-25 21:19:00 +0300

    add close()

--- step4/build.sh
+++ step4/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 gofmt -w .
+#go run -race *.go
 go run *.go
--- step4/main.go
+++ step4/main.go
@@ -2,20 +2,92 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func f(nums ...int) <-chan int {
+func gen(nums ...int) <-chan int {
 	out := make(chan int)
 	go func() {
 		for _, n := range nums {
 			out <- n
 		}
+		// close the channel when all the values have been sent
+		close(out)
 	}()
 	return out
 }
 
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		fmt.Println("done")
+		close(out)
+	}()
+	return out
+}
+
+//func merge(cs ...<-chan int) <-chan int {
+//	var wg sync.WaitGroup
+//
+//	out := make(chan int)
+//	wg.Add(len(cs))
+//
+//	for _, c := range cs {
+//		go func(c <-chan int) {
+//			for n := range c {
+//				out <- n
+//			}
+//			wg.Done()
+//		}(c)
+//	}
+//
+//	go func() {
+//		wg.Wait()
+//		close(out)
+//	}()
+//
+//	return out
+//}
+
 func main() {
-	fmt.Println("=== beg")
-	f(10, 20, 30)
-	fmt.Println("=== end")
+	in := gen(2, 3, 4, 5)
+
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(in)
+	c2 := sq(in)
+	c3 := sq(in)
+
+	// Consume the merged output from c1 and c2.
+	for n := range merge(c1, c2, c3) {
+		fmt.Println(n) // 4 then 9, or 9 then 4
+	}
 }
